63026f5cc284e6132443a66d2e622ecac2785279
guykamty <guykamty@gmail.com>
2024-12-06 15:46:59 +0700

    lab8_3_complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout << "Enter your age: ";
+    cin >> a;
+
+    if (a < 25) {
+        cout << "Enter your height: ";
+        cin >> c;
+
+        if (c < 100) {
+            cout << "Your character = Chopper";
+        } else if (c < 180) {
+            cout << "Your character = Usopp";
+        } else {
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if (b > 1100000000) {
+                cout << "Your character = Zoro";
+            } else {
+                cout << "Your character = Sanji";
+            }
+        }
+    } else if (a < 60) {
+        cout << "Enter your bounty: ";
+        cin >> b;
+        if (b > 500000000) {
+            cout << "Your character = Franky";
+        } else {
+            cout << "Your character = Jinbe";
+        }
+    } else {
+        cout << "Your character = Brook";
+    }
+
+    return 0;
+}
